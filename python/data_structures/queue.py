53e48d78936bcb9cbb47cd708daa0c83414f4c04
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-04-29 19:35:40 -0700

    error handling, 10 test passing

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,6 +1,7 @@
 from data_structures.linked_list import Node
 from data_structures.invalid_operation_error import InvalidOperationError
 
+
 class Queue:
     """
     Put docstring here
@@ -20,3 +21,20 @@ class Queue:
 
         if not self.front:
             self.front = self.rear
+
+    def dequeue(self):
+
+        if not self.front:
+            raise InvalidOperationError
+
+        prior_front = self.front
+        self.front = prior_front.next
+
+        return prior_front.value
+
+    def peek(self):
+
+        if not self.front:
+            raise InvalidOperationError
+
+        return self.front.value

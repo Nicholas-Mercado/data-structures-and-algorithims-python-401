31839d8953c1bb5eea14bee5daa2ca59030f065a
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-02 14:04:25 -0700

    created out/in stack

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,12 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.inbox = Stack()
+        self.outbox = Stack()
+
+    def enqueue(self, value):
+        self.inbox.push(value)
+
+    def dequeue(self):
+        pass

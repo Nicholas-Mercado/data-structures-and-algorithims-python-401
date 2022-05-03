87e26c4b44725da51c0acbd3a3d4efd3f5872488
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-03 13:39:26 -0700

    first setup, one test pass

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,12 +2,30 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
 
+    def __init__(self):
+        self.dog_queue = Queue()
+        self.cat_queue = Queue()
+
+    def enqueue(self, name):
+        self.cat_queue.enqueue(name)
+
+    def dequeue(self, name):
+        # if self.name == "cat":
+
+            return self.cat_queue.dequeue()
 
 class Dog:
-    pass
 
+    def __init__(self, name="dog"):
+        self.name = name
 
 class Cat:
-    pass
+
+    def __init__(self, name="cat"):
+        self.name = name
+
+
+# set up two queues
+# dog queue, cat queue
+#

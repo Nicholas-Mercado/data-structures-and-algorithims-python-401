50c6fecd52a7bc6bc0fb8586450842a1727e4837
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-04 14:36:09 -0700

    added ) logic, all test passing

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -6,7 +6,7 @@ def multi_bracket_validation(string):
     stack = Stack()
 
     for char in string:
-        if (char == ']' or char == '}') and stack.is_empty():
+        if (char == ']' or char == '}' or char == ')') and stack.is_empty():
             return False
 
         if char == '[':
@@ -15,10 +15,14 @@ def multi_bracket_validation(string):
         if char == '{':
             stack.push('}')
 
-        if (char == ']' or char == '}'):
+        if char == '(':
+            stack.push(')')
+
+        if (char == ']' or char == '}' or char == ')'):
 
             if stack.top.value == char:
                 stack.pop()
             else:
                 return False
+
     return True

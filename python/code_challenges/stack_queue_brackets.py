b36ca561b9a194730eeafe6fdb26f805d45ef3e1
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-04 13:51:13 -0700

    first test pass

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,23 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+
+    stack = Stack()
+
+    for char in string:
+        if char == '[':
+            stack.push(']')
+
+        if char == ']':
+            if stack.top:
+                if stack.top.value == char:
+                    stack.pop()
+                else:
+                    return False
+            
+    return True
+
+
+
+

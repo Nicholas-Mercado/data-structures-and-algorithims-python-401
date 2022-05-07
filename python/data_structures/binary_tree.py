39f0f3e60f41525019a1b830919f3de1402b0757
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-07 12:56:33 -0700

    created preorder, test pass

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,3 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
 class BinaryTree:
     """
     Put docstring here
@@ -5,15 +12,26 @@ class BinaryTree:
 
     def __init__(self):
         self.root = None
+        self.ordered_values = []
+
+    def pre_order(self):
+
+        def climb(root, values):
+
+            if not root:
+                return
+
+            values.append(root.value)
+
+            climb(root.left, values)
+
+            climb(root.right, values)
+
+        climb(self.root, self.ordered_values)
+
+        return self.ordered_values
 
 
-    def pre_order(self, value):
-
-        pass
 
 
-class Node:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+

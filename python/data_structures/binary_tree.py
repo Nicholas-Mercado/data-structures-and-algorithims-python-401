8f6fcf6bc51434d0d3bd65a3c7d8a716d7ca06d0
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-07 13:09:49 -0700

    created in order

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -32,6 +32,23 @@ class BinaryTree:
         return self.ordered_values
 
 
+    def in_order(self):
 
+        def climb(root, values):
+
+            if not root:
+                return
+
+            climb(root.left, values)
+
+            values.append(root.value)
+
+            climb(root.right, values)
+
+        climb(self.root, self.ordered_values)
+
+        return self.ordered_values
+
+    
 
 
--- python/tests/data_structures/test_binary_tree.py
+++ python/tests/data_structures/test_binary_tree.py
@@ -33,7 +33,7 @@ def test_pre_order(tree):
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_in_order(tree):
     actual = tree.in_order()
     expected = ["d", "b", "e", "a", "f", "c", "g"]  # left, root, right
@@ -69,54 +69,3 @@ def tree():
 
     return tree
 
-# import pytest
-# from data_structures.binary_tree import BinaryTree, Node
-
-
-# def test_exists():
-#     assert BinaryTree
-
-
-# # @pytest.mark.skip("TODO")
-# def test_pre_order(tree):
-#     actual = []
-#     tree.pre_order(actual.append)
-#     expected = ["a", "b", "d", "e", "c", "f", "g"]
-#     assert actual == expected
-
-
-# @pytest.mark.skip("TODO")
-# def test_in_order(tree):
-#     actual = []
-#     tree.in_order(actual.append)
-#     expected = ["d", "b", "e", "a", "f", "c", "g"]
-#     assert actual == expected
-
-
-# @pytest.mark.skip("TODO")
-# def test_post_order(tree):
-#     actual = []
-#     tree.post_order(actual.append)
-#     expected = ["d", "e", "b", "f", "g", "c", "a"]
-#     assert actual == expected
-
-
-# @pytest.fixture
-# def tree():
-#     """
-#           a
-#       b      c
-#     d  e    f  g
-#     """
-
-#     tree = BinaryTree()
-
-#     tree.root = Node("a")
-#     tree.root.left = Node("b")
-#     tree.root.right = Node("c")
-#     tree.root.left.left = Node("d")
-#     tree.root.left.right = Node("e")
-#     tree.root.right.left = Node("f")
-#     tree.root.right.right = Node("g")
-
-#     return tree

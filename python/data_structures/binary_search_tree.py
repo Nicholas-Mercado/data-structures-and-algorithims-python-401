deec8b59fec61a478ec4c52fc21809970c90a3d4
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-07 14:15:11 -0700

    created left add logic

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -8,10 +8,27 @@ class BinarySearchTree(BinaryTree):
 
     def add(self, value):
 
+        def climb(root, new_node):
+
+            if not root:
+                return
+
+            if new_node.value < root.value:
+                if root.left:
+                    climb(root.left, new_node)
+                else:
+                    root.left = new_node
+
+        node = Node(value)
+
         if not self.root:
-            self.root = Node(value)
+            self.root = node
+            return
+
+
+        climb(self.root, node)
 
 
 
 
-        
+
--- python/tests/data_structures/test_binary_search_tree.py
+++ python/tests/data_structures/test_binary_search_tree.py
@@ -24,7 +24,7 @@ def test_add_to_empty():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_add_left():
     tree = BinarySearchTree()
     tree.add(10)

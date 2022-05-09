9b0b920f019c2b7c9c7f89c2c381288286f08864
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-09 14:59:56 -0700

    all test passing

--- python/tests/code_challenges/test_tree_max.py
+++ python/tests/code_challenges/test_tree_max.py
@@ -13,3 +13,17 @@ def test_max_val():
     expected = 30
 
     assert actual == expected
+
+
+def test_max_val_deep():
+    tree = BinaryTree()
+    tree.root = Node(10)
+    tree.root.left = Node(30)
+    tree.root.right = Node(-7)
+    tree.root.right.right = Node(100)
+    tree.root.right.left = Node(44)
+
+    actual = tree.find_maximum_value()
+    expected = 100
+
+    assert actual == expected

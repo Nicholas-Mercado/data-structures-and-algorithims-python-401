ecb7850e28b2272a65b93a6348740ec615c52b18
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-10 14:39:12 -0700

    six test passing

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -5,7 +5,6 @@ from data_structures.queue import Queue
 
 def breadth_first(tree):
     final_list = []
-
     if tree is None:
         return final_list
 
@@ -18,5 +17,10 @@ def breadth_first(tree):
         temp_node = queue.dequeue()
         final_list.append(temp_node.value)
 
+        if temp_node.left is not None:
+            queue.enqueue(temp_node.left)
+            
+        if temp_node.right is not None:
+            queue.enqueue(temp_node.right)
 
     return final_list

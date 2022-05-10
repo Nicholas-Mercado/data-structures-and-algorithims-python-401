8d2bdaf828917b9f3f5f0ff7219138eb3452b895
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-10 14:38:34 -0700

    added add logic

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -14,10 +14,39 @@ class BinaryTree:
     Put docstring here
     """
 
-    def __init__(self):
-        self.root = None
+    def __init__(self, root=None, values=None):
+        self.root = root
         self.ordered_values = []
         self.max = 0
+        if values:
+            for value in values:
+                self.add(value)
+
+    def add(self, value):
+
+        node = Node(value)
+
+        if not self.root:
+            self.root = node
+            return
+
+        breadth = Queue()
+
+        breadth.enqueue(self.root)
+
+        while not breadth.is_empty():
+            front = breadth.dequeue()
+            if not front.left:
+                front.left = node
+                return
+            else:
+                breadth.enqueue(front.left)
+
+            if not front.right:
+                front.right = node
+                return
+            else:
+                breadth.enqueue(front.right)
 
     def pre_order(self):
 
@@ -91,28 +120,4 @@ class BinaryTree:
 
         return self.max
 
-    def add(self, value):
 
-        node = Node(value)
-
-        if not self.root:
-            self.root = node
-            return
-
-        breadth = Queue()
-
-        breadth.enqueue(self.root)
-
-        while not breadth.is_empty():
-            front = breadth.dequeue()
-            if not front.left:
-                front.left = node
-                return
-            else:
-                breadth.enqueue(front.left)
-
-            if not front.right:
-                front.right = node
-                return
-            else:
-                breadth.enqueue(front.right)

2332789b482ba9b8688dbb7a62004b0d1b8eb42f
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-11 15:15:39 -0700

    whiteboard code

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,20 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
+from data_structures.kary_tree import KaryTree, Node
+
+def fizz_buzz_tree(tree):
+
+    queue = Queue()
+    fizz_buzz_tree = KaryTree(tree.root)
+    queue.enqueue(tree.root)
+
+    while not queue.is_empty():
+
+        node = queue.dequeue()
+
+        node.value = is_fizz_buzz(node.value)
+        for child in node.children:
+            queue.enqueue(child)
+
+    return fizz_buzz_tree
 
 
-def fizz_buzz_tree():
-    pass

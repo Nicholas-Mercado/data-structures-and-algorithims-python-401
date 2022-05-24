0f9e95488dcd9a68762f764b770578fd83ecbbad
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-24 13:35:06 -0700

    transfer pusedo code merge_sort

--- python/code_challenges/merge_sort.py
+++ python/code_challenges/merge_sort.py
@@ -1,4 +1,15 @@
 
 
-def merge_sort():
+def merge_sort(lst):
+    n = len(lst)
+
+    if n > 1:
+        mid = n // 2
+        left = lst[0:mid]
+        right = lst[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        merge(left, right, lst)
+
+def merge(left, right, lst):
     pass

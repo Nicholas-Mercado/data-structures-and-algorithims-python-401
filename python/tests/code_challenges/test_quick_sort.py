c4c6668a1155487aa6842dfa809718c46af891a7
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-25 08:34:08 -0700

    setup blog

---
+++ python/tests/code_challenges/test_quick_sort.py
@@ -0,0 +1,33 @@
+import pytest
+from code_challenges.quick_sort import quick_sort
+
+
+def test_insertion_sort_full_sort():
+    arr = [6,5,4,3,2,1]
+    actual = quick_sort(arr)
+    expected = [1,2,3,4,5,6]
+    assert actual == expected
+
+def test_insertion_sort_reverse_sorted():
+    arr = [20,18,12,8,5,-2]
+    actual = quick_sort(arr)
+    expected = [-2,5,8,12,18,20]
+    assert actual == expected
+
+def test_insertion_sort_nearly_sorted():
+    arr = [2,3,5,7,13,11]
+    actual = quick_sort(arr)
+    expected = [2,3,5,7,11,13]
+    assert actual == expected
+
+def test_insertion_sort_empty():
+    arr = []
+    actual = quick_sort(arr)
+    expected = []
+    assert actual == expected
+
+def test_insertion_lots_off_zeros_and_a_one():
+    arr = [1,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    actual = quick_sort(arr)
+    expected = [0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+    assert actual == expected
--- sorting/quick/README.md
+++ sorting/quick/README.md
@@ -1,1 +1,17 @@
-# Blog Notes: Quick Sort
+# Quick Sort
+
+## Challenge Summary
+
+Review the merge Sort pseudocode, Create a blog post explaining how the code works. Take the pseudocode and create working python code with tests covering.
+
+## Whiteboard Process
+
+[Blog Post](sorting/merge/BLOG.md)
+
+## Approach & Efficiency
+
+### Quick Sort Big 0
+
+**Time Complexity** ---> Best case: θ(nLogn)/ Worst case: 0(n2)
+
+**Space Complexity** ---> 0(1) Because

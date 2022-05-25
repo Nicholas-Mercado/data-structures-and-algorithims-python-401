156ad0b797c82c80ef5e4edc5d9d5e0df529df6c
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-25 16:57:01 -0700

    add up numerals, two test passing

--- python/code_challenges/roman_numerals.py
+++ python/code_challenges/roman_numerals.py
@@ -1,2 +1,7 @@
 def roman_numerals(roman):
-    pass
+    numerals_values = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1,}
+    sum = 0
+    for i in range(len(roman)):
+        value = numerals_values[roman[i]]
+        sum += value
+    return sum
--- python/tests/code_challenges/test_roman_numerals.py
+++ python/tests/code_challenges/test_roman_numerals.py
@@ -3,3 +3,15 @@ from code_challenges.roman_numerals import roman_numerals
 
 def test_exists():
     assert roman_numerals
+
+def test_only_add():
+    roman = 'MDC'
+    actual = roman_numerals(roman)
+    expected = 1600
+    assert actual == expected
+
+def test_only_add_2():
+    roman = 'MDCI'
+    actual = roman_numerals(roman)
+    expected = 1601
+    assert actual == expected

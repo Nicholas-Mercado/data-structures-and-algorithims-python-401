c826f8904997c576574c47b13edefa6546b2d93b
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-26 13:28:16 -0700

    Can handle capitols, 8 test passing

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,15 +1,18 @@
 # from data_structures.hashtable import Hashtable
-# string_1 = "Once upon a time, there was a brave princess who..."
+# string_1 = "apple banana BANANA apple"
+
 
 def first_repeated_word(str):
-    keys = str.split(' ')
+    lower_string = (str).lower()
+    keys = lower_string.split(' ')
     word_dict = dict.fromkeys(keys, 0)
     for word in keys:
+        # print(word)
         word_dict[word] = word_dict.get(word, 0) + 1
         if word_dict[word] == 2:
             return word
 
-    print(word_dict)
+    # print(word_dict)
     return
 
 

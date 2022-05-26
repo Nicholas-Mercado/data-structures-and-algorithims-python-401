e1ea573c72ba35e671688c962e2bcd637c283f68
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-26 16:22:49 -0700

    clean code

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -3,18 +3,19 @@
 import re
 
 def first_repeated_word(str):
+    # clean up string for dict keys
     lower_string = (str).lower()
-    lower_string = lower_string.strip()
     lower_string = re.sub(r'\n', '', lower_string)
     keys = lower_string.split(' ')
     stripped_words = []
 
+    # remove special characters from each list item
     for word in keys:
         text = re.sub(r"[!?',;.]", '', word)
         stripped_words.append(text)
-
+    # Create dict
     word_dict = dict.fromkeys(stripped_words, 0)
-
+    # increment key values and check for value of 2
     for word in stripped_words:
         word_dict[word] = word_dict.get(word, 0) + 1
         if word and word_dict[word] == 2:

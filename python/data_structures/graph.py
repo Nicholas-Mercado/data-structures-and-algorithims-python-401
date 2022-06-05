0774e58611bf318474c463e67e8a24baa803ab34
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-06-04 17:06:09 -0700

    pass multiple nodes tests

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,20 +1,39 @@
 class Graph:
-    """
-    Put docstring here
-    """
+    '''Implement Graph data structure with following methods
+
+    Methods:
+
+        add_node(self, value)
+        size(self)
+        get_nodes(self)
+        add_edge(self, start_vertex, end_vertex, weight=0)
+        get_neighbors(self, vertex)
+        
+    '''
 
     def __init__(self):
-       self._adjacency_list = {}
+        self._adjacency_list = {}
 
     def add_node(self, value):
+        ''' Adds node to graph returns vertex
+
+        Arg: value
+        '''
         vertex = Vertex(value)
         self._adjacency_list[vertex] = []
         return vertex
 
     def size(self):
+
         return len(self._adjacency_list)
 
     def get_nodes(self):
+        """
+        Description of get_nodes
+
+        Args: None
+
+        """
         return self._adjacency_list.keys()
 
     def add_edge(self, start_vertex, end_vertex, weight=0):
@@ -22,18 +41,22 @@ class Graph:
             raise KeyError()
 
         edge = Edge(end_vertex, weight)
-
+        # appending to the list of _adjacency_list[start_vertex] not the Dict _adjacency_list
         self._adjacency_list[start_vertex].append(edge)
 
     def get_neighbors(self, vertex):
         return self._adjacency_list[vertex]
 
+print(Graph.__doc__)
 
 class Vertex:
     def __init__(self, value):
         self.value = value
 
+
 class Edge:
     def __init__(self, vertex, weight):
         self.vertex = vertex
         self.weight = weight
+
+
--- python/tests/data_structures/test_graph.py
+++ python/tests/data_structures/test_graph.py
@@ -80,7 +80,7 @@ def test_bouquet():
     assert neighbors[0].weight == 10
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_add_edge_interloper_start():
 
     graph = Graph()
@@ -93,7 +93,7 @@ def test_add_edge_interloper_start():
         graph.add_edge(start, end)
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_add_edge_interloper_end():
 
     graph = Graph()
@@ -124,10 +124,34 @@ def test_get_nodes():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_get_neighbors():
 
     graph = Graph()
+
+    banana = graph.add_node("banana")
+
+    apple = graph.add_node("apple")
+
+    graph.add_edge(apple, banana, 44)
+
+    neighbors = graph.get_neighbors(apple)
+
+    assert len(neighbors) == 1
+
+    neighbor_edge = neighbors[0]
+
+    assert neighbor_edge.vertex.value == "banana"
+
+    assert neighbor_edge.weight == 44
+
+def test_get_neighbors_two_deep():
+
+    graph = Graph()
+
+    graph.add_node("jim")
+
+    graph.add_node("rick")
 
     banana = graph.add_node("banana")
 
